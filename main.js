afd13359f532673a00a80901a2a3ae936759a515
Terrence Ryan <terry@terrenceryan.com>
2013-11-06 17:30:09 -0500

    Made a webservice so I could call it from an html page.

--- main.js
+++ main.js
@@ -2,17 +2,42 @@
 /*global define, require*/
 
 
-var miles_start = 72929;
-var miles_final = 0;
-var milage = require('./milage');
-var ical_url = "https://www.tripit.com/feed/ical/private/1AF53D51-488D505D31282AC36A65F4FD788BB039/tripit.ics";
+var http = require('http');
  
 
+var server = http.createServer(function (req, res) {
+    
+    var result = {}; 
+    result.start = 72929;
+    
+    var milage = require('./milage');
+    var ical_url = "https://www.tripit.com/feed/ical/private/1AF53D51-488D505D31282AC36A65F4FD788BB039/tripit.ics";
+    
+    
+    var serverCallback = function (report) {
+        var localRes = res; 
+        result.report = report;
+        result.final= calculateFuture(result.start, report);
+        localRes.writeHead(200, { 'Content-Type': 'application/json' })
+        localRes.end(JSON.stringify(result))
+    }
+    
+    milage(ical_url, serverCallback);
+    
+})
+server.listen(8000);
 
 
 
-milage(ical_url, function (report) {console.log(report);});
 
-
+function calculateFuture(start, report) {
+    var result =  Number(start);
+    
+    for (var i = 0; i < report.length; i++){
+        result = result + Number(report[i].milage); 
+        
+    }
+    return result;    
+}    
 
 
